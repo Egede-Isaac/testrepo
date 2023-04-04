b28df3da22ba4e52875326d41ececd4c5487a147
Egede-Isaac <98391491+Egede-Isaac@users.noreply.github.com>
2023-04-04 09:18:13 +0100

    Add files via upload

---
+++ print staements and user input.py
@@ -0,0 +1,15 @@
+print("Hello Baby", end= ' ')
+print("How are you today?")
+
+x = 5
+y = 2
+
+print("The value of x is {} and y is {}" .format(x,y))
+
+
+
+name = str( input("Hello there! What's your name?\n"))
+age = int (input("How old are you\n"))
+location = str (input("Where do you live?\n"))
+
+print ("Thank you! Your name is " + name+" and you are ",age," years old and you live in "+location)
